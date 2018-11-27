263aa706ddc74e1e22347d315ffb8908b1bec7da
braunsteind <braunstein93@gmail.com>
2018-11-27 21:36:51 +0200

    Shlomi

--- stopper_node.py
+++ stopper_node.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     rotation_speed = 0.5
     min_angle = -30
     max_angle = 30
+    min_dist_from_obstacle = 0.65
 
     if rospy.has_param('~forward_speed'):
         forward_speed = rospy.get_param('~forward_speed')
@@ -22,5 +23,7 @@ if __name__ == "__main__":
         min_angle = rospy.get_param('~min_angle')
     if rospy.has_param('~max_angle'):
         max_angle = rospy.get_param('~max_angle')
-    my_stopper = Stopper(forward_speed, rotation_speed, min_angle, max_angle)
+    if rospy.has_param('~min_dist_from_obstacle'):
+        min_dist_from_obstacle = rospy.get_param('~min_dist_from_obstacle')
+    my_stopper = Stopper(forward_speed, rotation_speed, min_angle, max_angle, min_dist_from_obstacle)
     my_stopper.start_moving()

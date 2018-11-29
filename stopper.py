86500bda76c3800a934f4ce653a0a20932ee03fb
braunsteind <braunstein93@gmail.com>
2018-11-29 21:00:46 +0200

    Fix

--- stopper.py
+++ stopper.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python
-#
-# stopper.py
-#
-#  Created on: Nov 13, 2017
-#      Author: Mika Barkan
-#
 
 PI = 3.1415926535897
 
@@ -15,37 +9,50 @@ from sensor_msgs.msg import LaserScan
 
 
 class Stopper(object):
+    # constructor
     def __init__(self, forward_speed, rotation_speed, min_angle, max_angle, min_dist_from_obstacle):
+        # extract info
         self.forward_speed = forward_speed
         self.rotation_speed = rotation_speed
-
-        # self.min_scan_angle = min_angle / 180 * math.pi
-        # self.max_scan_angle = max_angle / 180 * math.pi
-
         self.min_scan_angle = math.radians(min_angle)
         self.max_scan_angle = math.radians(max_angle)
-        self.min_dist_from_obstacle = 0.65
+        self.min_dist_from_obstacle = min_dist_from_obstacle
+        # default values
         self.direction = 1
-
         self.keep_moving = True
+
         self.command_pub = rospy.Publisher("/cmd_vel_mux/input/teleop", Twist, queue_size=10)
         self.laser_subscriber = rospy.Subscriber("scan", LaserScan, self.scan_callback, queue_size=1)
 
-        self.round = 25
+        self.round = 1
         self.angular_speed = rotation_speed * PI / 180
         self.relative_angle = self.round * PI / 180
+
+    def findDirection(self, ranges):
+        max_distance = 0
+        index = 0
+        # loop over ranges
+        for i in ranges:
+            # find the max distance
+            if ranges[i] > max_distance:
+                max_distance = ranges[i]
+                index = i
+        # return the direction to turn
+        if index < (len(ranges) / 2):
+            return 1
+        return -1
 
     def start_moving(self):
         rate = rospy.Rate(10)
 
         while not rospy.is_shutdown():
-
-            if not self.keep_moving:
-                self.rotate()
-            else:
+            if self.keep_moving:
                 self.move_forward()
+            else:
+                self.rotate()
             rate.sleep()
 
+    # handling the forward movement
     def move_forward(self):
         move_msg = Twist()
         move_msg.linear.x = self.forward_speed
@@ -55,40 +62,20 @@ class Stopper(object):
         scan_msg.angle_min = self.min_scan_angle
         scan_msg.angle_max = self.max_scan_angle
 
-        # initialize flags for discovering which direction is more free
-        left = -1
-        right = -1
-
-        for i in range(320):
-            if scan_msg.ranges[i] > self.min_dist_from_obstacle or math.isnan(scan_msg.ranges[i]):
-                left = i
-            elif scan_msg.ranges[639 - i] > self.min_dist_from_obstacle or math.isnan(scan_msg.ranges[639 - i]):
-                right = 639 - i
-
+        # go over the ranges
         for dist in scan_msg.ranges:
+            # obstacle found
             if dist < self.min_dist_from_obstacle:
+                # stop moving
                 self.keep_moving = False
-                if right != -1 and left != -1:
-                    if abs(320 - right) > abs(320 - left):
-                        self.direction = 1
-                    else:
-                        self.direction = -1
-                elif right != -1:
-                    self.direction = 1
-                else:
-                    self.direction = -1
-                break
-            else:
-                self.keep_moving = True
+                # find the direction
+                self.direction = self.findDirection(scan_msg.ranges)
 
+    # handling rotation
     def rotate(self):
-        # rot = Twist()
-        # rot.angular.z = self.direction * math.radians(25)
-        # self.command_pub.publish(rot)
-
         # initializing values
         rot = Twist()
-        rot.angular.z = abs(self.angular_speed)
+        rot.angular.z = self.direction * self.angular_speed
         t0 = rospy.Time.now().to_sec()
         current_angle = 0
 
@@ -97,3 +84,5 @@ class Stopper(object):
             self.command_pub.publish(rot)
             t1 = rospy.Time.now().to_sec()
             current_angle = self.angular_speed * (t1 - t0)
+        # keep moving
+        self.keep_moving = True
